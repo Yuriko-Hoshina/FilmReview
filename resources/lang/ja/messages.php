d74dde103a95823636cd7223b30cbd26ff9b9d30
Yuriko <utanotantei@gmail.com>
2022-04-14 14:00:33 +0000

    login

---
+++ resources/lang/ja/messages.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Login' => 'ログイン',
+    'E-Mail Address' => 'メールアドレス',
+    'Password' => 'パスワード',
+    'Remember Me' => 'ログイン情報を記憶する',
+    'Forgot Your Password?' => 'パスワードをお忘れですか？',
+    'Register' => '新規登録',
+    'Confirm Password' => 'パスワードの確認',
+    'Name' => 'ID',
+];
